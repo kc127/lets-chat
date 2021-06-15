c3e46956c371725b95a14bcd81c0b41548db2306
Kanchan Chauhan <kanchan827@gmail.com>
2021-06-14 22:29:57 -0400

    set up babel and webpack

---
+++ client/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hi React is working</h1>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  mode: 'development'
+}
