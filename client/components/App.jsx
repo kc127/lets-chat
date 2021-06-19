b8a99742c15f95b9e64c90ccc8c8d224bcc4a380
Kanchan Chauhan <kanchan827@gmail.com>
2021-06-19 19:46:35 -0400

    add socket.io

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,137 @@
-import React from 'react';
-import Form from './Form';
+import React, { useEffect, useRef, useState } from 'react';
 
-const App = () => (
-  <div className="container">
-    <header className="header">
-      <h1>Zoomish</h1>
-    </header>
-    <Form />
-  </div>
-);
+import { copyToClipboard } from 'react-copy-to-clipboard';
+import Peer from 'simple-peer';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:5001');
+
+function App() {
+  const [me, setMe] = useState('');
+  const [stream, setStream] = useState('');
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState('');
+  const [callerSignal, setCallerSignal] = useState(null);
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState('');
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState('');
+
+  /* allows us to reference our video */
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
+  /* componentDidMount */
+  useEffect(() => {
+    /* allows us to use webcam */
+    // eslint-disable-next-line no-undef
+    navigator.mediaDevices.getUserMedia({ vide: true, audio: true }).then((stream) => {
+      setStream(stream);
+      myVideo.current.srcObject = stream;
+    });
+
+    socket.on('me', (id) => {
+      setMe(id);
+    });
+
+    socket.on('callUser', (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
+  }, []);
+
+  /* call the user */
+  const callUser = (id) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream,
+    });
+
+    peer.on('signal', (data) => {
+      socket.emit('callUser', {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name,
+      });
+
+      /* other user's video */
+      peer.on('stream', (stream) => {
+        userVideo.current.srcObject = stream;
+      });
+
+      socket.on('callAccepted', (signal) => {
+        setCallAccepted(true);
+        peer.signal(signal);
+      });
+
+      /* when we end the call, we can disable the connectionRef */
+      connectionRef.current = peer;
+    });
+
+    /* answer the call */
+    const answerCall = () => {
+      setCallAccepted(true);
+      const peer1 = new Peer({
+        initiator: false,
+        trickle: false,
+        stream,
+      });
+
+      peer.on('signal', (data) => {
+        socket.emit('answerCall', {
+          signal: data,
+          to: caller,
+        });
+      });
+
+      peer.on('stream', (stream) => {
+        userVideo.current.srcObject = stream;
+      });
+
+      peer.signal(callerSignal);
+
+      connectionRef.current = peer;
+    };
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+    /* this ends our connection */
+    connectionRef.current.destroy();
+  };
+
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>Skypish</h1>
+      </header>
+      <div className="video-container">
+        <div className="video">
+          {stream && <video playsInline muted ref={myVideo} autoPlay />}
+        </div>
+        <div className="video">
+          {callAccepted && !callEnded
+            ? <video playsInline ref={userVideo} autoPlay /> : null}
+        </div>
+      </div>
+      <div className="myId">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          id="caller-id"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+    </div>
+  );
+}
 
 export default App;
--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const socketio = require('socket.io');
 
 const app = express();
-const server = require('http').createServer(app);
-const cors = require('cors');
-
-const io = require('socket.io')(server, {
+const server = http.createServer(app);
+const io = socketio(server, {
   cors: {
-    origin: '*',
+    origin: 'http://localhost:5001',
     methods: ['GET', 'POST'],
   },
 });
 
 app.use(express.json());
 app.use(cors());
+
+/* serve static folder */
 app.use(express.static(path.join(__dirname, '/public')));
+
+/* run when client connects */
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+
+  socket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('callUser', { signal: data.signalData, from: data.from, name: data.name });
+  });
+
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+});
 
 const PORT = process.env.PORT || 5001;
 
