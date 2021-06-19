2af2d890cc685262abb366a0a6f74b23a8cd8375
Kanchan Chauhan <kanchan827@gmail.com>
2021-06-19 13:09:07 -0400

    add username and room labels

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -14,8 +14,20 @@ const Form = () => (
         />
       </div>
       <div className="form-control">
-        
+        <label htmlFor="room">Room</label>
+        <select name="room" id="room">
+          <option value="Issue A">Issue A</option>
+          <option value="Issue B">Issue B</option>
+          <option value="Issue C">Issue C</option>
+          <option value="Issue D">Issue D</option>
+          <option value="Issue E">Issue E</option>
+          <option value="Issue F">Issue F</option>
+          <option value="Issue G">Issue G</option>
+          <option value="Issue H">Issue H</option>
+          <option value="Issue I">Issue I</option>
+        </select>
       </div>
+      <button type="submit" className="btn">Join Chat</button>
     </form>
   </div>
 );
